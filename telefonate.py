e2151d4a925753e88f01e99540d6c3cd92e1c9e8
rOlsid <142094668+rOlsid@users.noreply.github.com>
2026-05-11 19:01:39 +0200

    Update telefonate.py

--- telefonate.py
+++ telefonate.py
@@ -5,14 +5,14 @@ from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 
-account_sid = "AC851cdc148416c23c734d1dadaa565374"
-auth_token = "bdba3a98e99f7093408ba4b05ba57344"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 call = client.calls.create(
   url="http://demo.twilio.com/docs/voice.xml",
-  to="+355683090136",
-  from_="+13203728331"
+  to="+",
+  from_="+"
 )
 
 print(call.sid)
